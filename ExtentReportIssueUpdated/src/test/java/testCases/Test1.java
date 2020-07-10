313430fd907b4b83654c7ed41846265a9fc7e337
anshooarora <anshooarora@gmail.com>
2020-07-09 17:43:31 -0700

    listener usage

--- ExtentReportIssueUpdated/src/test/java/testCases/Test1.java
+++ ExtentReportIssueUpdated/src/test/java/testCases/Test1.java
@@ -4,10 +4,13 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.service.ExtentTestManager;
+import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
 
+@Listeners(ExtentITestListenerClassAdapter.class)
 public class Test1 {
 	
     static {
@@ -20,13 +23,13 @@ public class Test1 {
     @BeforeSuite
     public void beforeSuiteMethod() {
     	
-    	ExtentTestManager.getTest().info("Calling b4 suite method.");
+    	//ExtentTestManager.getTest().info("Calling b4 suite method.");
     }
     
 	@BeforeTest
 	public void b4Test() {
 		
-		ExtentTestManager.getTest().info("Calling b4 Test method.");
+		//ExtentTestManager.getTest().info("Calling b4 Test method.");
 		
 	}
 	
@@ -35,23 +38,24 @@ public class Test1 {
 	@BeforeMethod
 	public void initMethod() {
 		
-		ExtentTestManager.getTest().info("Starting BeforeMethod method.");
-		System.out.println("This is an initial method...");
+		//ExtentTestManager.getTest().info("Starting BeforeMethod method.");
+		//System.out.println("This is an initial method...");
 		
 	}
 	
 	@Test
 	public void testMethod() {
 		
-		ExtentTestManager.getTest().info("Starting test method.");
-		System.out.println("This is test a test Method.");
+		//ExtentTestManager.getTest().info("Starting test method.");
+		//System.out.println("This is test a test Method.");
+	    Assert.assertTrue(true);
 	}
 	
 	@Test
 	public void testMethod1() {
 		
-		ExtentTestManager.getTest().info("Starting test1 method.");
-		System.out.println("This is test1 a test Method.");
+		//ExtentTestManager.getTest().info("Starting test1 method.");
+		//System.out.println("This is test1 a test Method.");
 		Assert.assertTrue(false);
 	}
 
